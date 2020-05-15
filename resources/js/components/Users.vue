f908a01716b0783e8c4e4dc4ac163ad60f01b300
halomoan@gmail.com <N3tw0rkgithub>
2020-05-15 18:27:33 +0800

    master

---
+++ resources/js/components/Users.vue
@@ -0,0 +1,145 @@
+<template>
+    <div>
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Users</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item">
+                                <a href="#">Home</a>
+                            </li>
+                            <li class="breadcrumb-item active">Users</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            <!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <a
+                                    href
+                                    data-toggle="modal"
+                                    data-target="#addNew"
+                                >
+                                    <i class="fa fa-user-plus"></i>
+                                    Add New
+                                </a>
+
+                                <div class="card-tools">
+                                    <div
+                                        class="input-group input-group-sm"
+                                        style="width: 250px;"
+                                    >
+                                        <input
+                                            type="text"
+                                            name="table_search"
+                                            class="form-control float-right"
+                                            placeholder="Search"
+                                        />
+
+                                        <div class="input-group-append">
+                                            <button
+                                                type="submit"
+                                                class="btn btn-default"
+                                            >
+                                                <i class="fas fa-search"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-hover text-nowrap">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Name</th>
+                                            <th>Email</th>
+                                            <th>Type</th>
+                                            <th>Modify</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>183</td>
+                                            <td>John Doe</td>
+                                            <td>11-7-2014</td>
+                                            <td>
+                                                <span class="tag tag-success"
+                                                    >Approved</span
+                                                >
+                                            </td>
+                                            <td>
+                                                <a href class="fa fa-edit"></a>
+                                                /
+                                                <a
+                                                    href
+                                                    class="fa fa-trash text-red"
+                                                ></a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </section>
+        <div
+            class="modal fade"
+            id="addNew"
+            tabindex="-1"
+            role="dialog"
+            aria-labelledby="addNewLabel"
+            aria-hidden="true"
+        >
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addNewLabel">Add New</h5>
+                        <button
+                            type="button"
+                            class="close"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                        >
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">...</div>
+                    <div class="modal-footer">
+                        <button
+                            type="button"
+                            class="btn btn-secondary"
+                            data-dismiss="modal"
+                        >
+                            Close
+                        </button>
+                        <button type="button" class="btn btn-primary">
+                            Create
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {};
+</script>
